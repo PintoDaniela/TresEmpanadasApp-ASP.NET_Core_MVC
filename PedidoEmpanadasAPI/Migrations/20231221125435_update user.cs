7c7edbeb2c0881e9ccfa8579e8421acfb254c36e
PintoDaniela <danielafpinto90@gmail.com>
2024-01-19 12:49:37 -0300

    se cambia paleta de colores

---
+++ PedidoEmpanadasAPI/Migrations/20231221125435_update user.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PedidoEmpanadasAPI.Migrations
+{
+    public partial class updateuser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "GroupName",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GroupName",
+                table: "User");
+        }
+    }
+}
---
+++ PedidoEmpanadasAPI/Migrations/20240119124450_se revierte groupname.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PedidoEmpanadasAPI.Migrations
+{
+    public partial class sereviertegroupname : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
